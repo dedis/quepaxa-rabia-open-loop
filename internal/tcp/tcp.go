a96ca7fbbb36631d2a7c1177bf188ba6f4c28296
pasindu <pasindu.13@cse.mr.ac.lk>
2022-07-15 11:46:12 +0200

    updated network binding

--- internal/tcp/tcp.go
+++ internal/tcp/tcp.go
@@ -1,17 +1,17 @@
 /*
-    Copyright 2021 Rabia Research Team and Developers
+   Copyright 2021 Rabia Research Team and Developers
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+      http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 /*
 	The tcp package defines ClientTCP, ProxyTCP, and NetTCP objects, which are the TCP communication component of a
@@ -34,6 +34,7 @@ import (
 	"net"
 	. "rabia/internal/config"
 	. "rabia/internal/message"
+	"strings"
 	"sync"
 	"time"
 )
@@ -203,9 +204,17 @@ type ProxyTCP struct {
 	Writers  []*bufio.Writer
 }
 
+/*
+	Returns the port part of the ip:port
+*/
+
+func getPort(address string) string {
+	return strings.Split(address, ":")[1]
+}
+
 // Allocates the ProxyTCP object without accepting connections from its clients
 func ProxyTcpInit(Id uint32, ProxyIp string, ToProxy chan Command) *ProxyTCP {
-	listener, err := net.Listen("tcp", ProxyIp)
+	listener, err := net.Listen("tcp", "0.0.0.0:"+getPort(ProxyIp))
 	if err != nil {
 		panic(err)
 	}
@@ -355,7 +364,7 @@ type NetTCP struct {
 }
 
 func NetTCPInit(Id uint32, NetIp string) *NetTCP {
-	listener, err := net.Listen("tcp", NetIp)
+	listener, err := net.Listen("tcp", "0.0.0.0:"+getPort(NetIp))
 	if err != nil {
 		panic(err)
 	}
--- roles/controller/controller.go
+++ roles/controller/controller.go
@@ -1,17 +1,17 @@
 /*
-    Copyright 2021 Rabia Research Team and Developers
+   Copyright 2021 Rabia Research Team and Developers
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+      http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 /*
 	The controller package defines the struct and functions of the benchmark controller. The controller (1) informs all
@@ -63,6 +63,7 @@ import (
 	. "rabia/internal/config"
 	. "rabia/internal/message"
 	"rabia/internal/tcp"
+	"strings"
 	"time"
 )
 
@@ -98,10 +99,18 @@ func RunController() {
 }
 
 /*
+	Returns the port part of the ip:port
+*/
+
+func getPort(address string) string {
+	return strings.Split(address, ":")[1]
+}
+
+/*
 	Initialize the controller
 */
 func controllerInit() *Controller {
-	listener, err := net.Listen("tcp", Conf.ControllerAddr)
+	listener, err := net.Listen("tcp", "0.0.0.0:"+getPort(Conf.ControllerAddr))
 	if err != nil {
 		panic(fmt.Sprint("should not happen", err))
 	}
